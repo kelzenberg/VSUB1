01874b28312af5b901ae8afa6ef47414d1a5a85c
kroko <root.1drone@gmail.com>
2017-10-13 18:49:39 +0200

    added comments

--- src/FibonacciCalc.java
+++ src/FibonacciCalc.java
@@ -1,25 +1,40 @@
+/**
+ * FibonacciCalc
+ * Calculates n-th fibonacci number.
+ *
+ * @author
+ * Jannis Rieger
+ * Steffen Ansorge
+ * Nicolai Brandt
+ *
+ * @version 1.0
+ */
+
 public class FibonacciCalc {
-    /**
-     * @author
-     */
+
     private FibonacciCalc() {
     }
 
+    /**
+     *
+     * @param num the desired fibonacci number.
+     * @return calculated fibonacci number.
+     */
     public static long calculate(int num) {
 
         if (num <= 0)
             return 0;
-        else if (num == 1)  //n=1
+        else if (num == 1)
             return 1;
-        else if (num == 2)  //n=2
+        else if (num == 2)
             return 1;
-        else {              //n=3
+        else {
             int i = 3;
-            long n1 = i - 1;   //n-1
-            long n2 = i - 2;   //n-2
+            long n1 = i - 1;
+            long n2 = i - 2;
             long out = 1;
 
-            while (i <= num) {   //von n=3 bis num
+            while (i <= num) {
                 out = n1 + n2;
                 n2 = n1;
                 n1 = out;
@@ -31,5 +46,4 @@ public class FibonacciCalc {
             return out;
         }
     }
-
 }
